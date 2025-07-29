0a046bce2c90201097d5b5c0f49bad26e2865ac1
Sarah Kirchner <sarah.kirchner@sulzer.de>
2025-07-29 15:37:28 +0200

    fix: 🚐  remove outline by button

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export const App: React.FC = (): React.JSX.Element => {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
 
   return (
     <div className="bg-black text-white h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold tracking-wide hover:bg-gray-500 p-2 cursor-pointer">Hello</h1>
       <p>Test</p>
-      <button onClick={ipcHandle} className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4">
+      <button
+        onClick={ipcHandle}
+        className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 outline-none"
+      >
         Ping
       </button>
     </div>
-  )
-}
+  );
+};
